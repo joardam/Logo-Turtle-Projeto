482403dabb2d45799a15273613a8826f9670b2ae
Victor Ferreira <victorjcferreira462@gmail.com>
2023-04-15 17:43:30 -0300

    Mudanças Vitu#1

--- Mecânica.py
+++ Mecânica.py
@@ -2,6 +2,7 @@ import curses
 from curses import wrapper
 
 def main(stdscr):
+    curses.curs_set(0) # Oculta o cursor do terminal
     stdscr.clear()
     stdscr.box('|','-')
     stdscr.refresh()

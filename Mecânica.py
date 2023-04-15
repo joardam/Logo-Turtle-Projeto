9260eead5532fed14627b3beb12d80a3550c215c
Victor Ferreira <victorjcferreira462@gmail.com>
2023-04-15 17:42:58 -0300

    Mudanças Vitu#1

--- Mecânica.py
+++ Mecânica.py
@@ -2,14 +2,15 @@ import curses
 from curses import wrapper
 
 def main(stdscr):
-    curses.curs_set(0) # Oculta o cursor do terminal
     stdscr.clear()
     stdscr.box('|','-')
     stdscr.refresh()
 
-    while True: # Loop infinito
+
+    # Apertar "q" para sair
+    while True: 
         key = stdscr.getch()
-        if key == ord('q'): # Sai do loop se o usuário pressionar a tecla 'q'
+        if key == ord('q'): 
             break
 
 wrapper(main)
